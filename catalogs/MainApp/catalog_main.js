8b95122ea460c344be00fa8cc0c300f46027e0bb
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-09-06 15:58:59 +0900

    Fix the bug.

--- catalogs/MainApp/catalog_main.js
+++ catalogs/MainApp/catalog_main.js
@@ -8,11 +8,13 @@ function start_quest(data) {
         api.start_quest({ 
             "author":__quest_author, 
             "permlink":__quest_permlink 
-        });    
+        }); 
     });
 
     __quest_author   = data["author"];
     __quest_permlink = data["permlink"];
+
+    document.value("QUEST_MODE", true);
 }
 
 function finish_quest(data) {
